2222578eba4c607ae8339ba134058f3181d34fb6
Vladislav <vladikul98@gmail.com>
2023-08-22 13:59:27 +0300

    Task for Hibernate 2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -10,8 +10,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDaoHibernateImpl userService = new UserDaoHibernateImpl();
+        UserService service = new UserServiceImpl();
 
+        service.createUsersTable();
+        service.saveUser("Name 1", "Lastname 1", (byte) 13);
+        service.saveUser("Name 2", "Lastname 2", (byte) 54);
+        service.saveUser("Name 3", "Lastname 3", (byte) 89);
+        service.saveUser("Name 4", "Lastname 4", (byte) 25);
 
+        service.getAllUsers().forEach(System.out::println);
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
